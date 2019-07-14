9b8fb237fdf0923fd68204291f0a8790986ae38f
XanderNelson <47230282+XanderNelson@users.noreply.github.com>
2019-07-14 14:32:48 -0600

    Add files via upload

--- Thunderstone Capital/javaScript/thunderstoneJS.js
+++ Thunderstone Capital/javaScript/thunderstoneJS.js
@@ -1,3 +1,9 @@
 // if(window.scrollY==0){
 //   document.body.style.backgroundColor = "red";
 // }
+// $(window).scroll(function(){
+// 	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+// });
+// $(window).scroll(function(){
+// 	$('a').toggleClass('scrolled', $(this).scrollTop() > 50);
+// });
